867755ab3be30ceb7c6477098ab82b6332520918
blockheadbandit <steptoereece@gmail.com>
2026-01-13 12:19:39 +0000

    grammar updates

--- src/pages/Projectspage.js
+++ src/pages/Projectspage.js
@@ -30,13 +30,13 @@ export default function Projects() {
 	  <div className={styles.MainPanel}>
 		<div>
 			<h1>My Projects</h1>
-			<p>Here are all the projects I have worked on and uploaded to the public, feel free to take a look at the code or download a release</p>
+			<p>Here are all the projects I have worked on and uploaded to the public, feel free to take a look at the code or download a release.</p>
 			<ProjectEntry name="Portfolio Website"
 				desc="I made this website with React, you can find the project repo below."
 				repo="https://github.com/blockheadbandit/portfolio/tree/master"
 			/>
 			<ProjectEntry name="Ktempsensor" 
-				desc="This project started because I disliked having to use nvidia-smi and top/htop to view the temperature of my CPU and GPU currently it is CLI only however I plan on adding a GUI at some point"
+				desc="This project started because I disliked having to use nvidia-smi and top/htop to view the temperature of my CPU and GPU. Currently, it is CLI only; however, I plan on adding a GUI at some point."
 				repo="https://github.com/blockheadbandit/Ktempsensor" 
 				release="https://github.com/blockheadbandit/Ktempsensor/releases"
 			/>
